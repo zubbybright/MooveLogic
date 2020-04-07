734c81dd86144ac743a04371090f480cced23a8c
Zubby <zubbybrightson@gmail.com>
2020-04-07 18:57:13 +0100

    test

--- app/Http/Controllers/TripsController.php
+++ app/Http/Controllers/TripsController.php
@@ -203,8 +203,8 @@ class TripsController extends BaseController
 
                 $rider =  User::where('user_type', 'RIDER')
                 ->where('on_a_ride', 0)
-                ->whereBetween('latitude', [$data['latitude']- (10 * 0.018), $data['latitude']+ (10 * 0.018)])
-                ->whereBetween('longitude', [$data['longitude']- (10 * 0.018), $data['longitude']+ (10 * 0.018)])
+                // ->whereBetween('latitude', [$data['latitude']- (10 * 0.018), $data['latitude']+ (10 * 0.018)])
+                // ->whereBetween('longitude', [$data['longitude']- (10 * 0.018), $data['longitude']+ (10 * 0.018)])
                 ->inRandomOrder()
                 ->take(1)
                 ->first();
