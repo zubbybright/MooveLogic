78a127b91bba63982e677293dd05386641bb3a27
Zubby <zubbybrightson@gmail.com>
2020-08-25 16:57:20 +0100

    sign in with either email or phone number

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\BaseController;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends BaseController
 {
@@ -23,24 +24,36 @@ class LoginController extends BaseController
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function login()
+    public function login(Request $request)
     {
-        $credentials = request(['phone_number', 'password']);
-        if (! $token = auth()->attempt($credentials)) {
-            return $this->sendError("Invalid phone number or password!", "Invalid phone number or password!");
+        $input = $request->all();
+  
+        $this->validate($request, [
+            'email' => 'nullable',
+            'password' => 'required',
+            
+        ]);
+
+        $fieldType = filter_var($request->email, FILTER_VALIDATE_EMAIL) ? 'email' : 'phone_number';
+
+        if($token = auth()->attempt(array($fieldType => $input['email'], 'password' => $input['password']))){
+            
+            $data  = [
+                'user' => auth()->user(),
+                'profile' => auth()->user()->profile,
+                'token' => [
+                    'access_token' => $token,
+                    'token_type' => 'bearer',
+                    'expires_in' => auth()->factory()->getTTL()
+                ]
+            ];
+    
+            return $this->sendResponse($data, 'Login successful.');
+        } else {
+
+            return $this->sendError('Invalid Login Credentials.', 'Invalid Login Credentials.');
         }
 
-        $data  = [
-            'user' => auth()->user(),
-            'profile' => auth()->user()->profile,
-            'token' => [
-                'access_token' => $token,
-                'token_type' => 'bearer',
-                'expires_in' => auth()->factory()->getTTL()
-            ]
-        ];
 
-        return $this->sendResponse($data, 'Login successful.');
     }
-
 }
