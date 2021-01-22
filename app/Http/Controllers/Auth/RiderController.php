b24b3fedb3fcf9c80fcb2f905c0cb80d77429769
Oyesola Ogundele <oyekunmi@yahoo.com>
2021-01-22 14:11:43 +0400

    cicd: modify pipeline

--- app/Http/Controllers/Auth/RiderController.php
+++ app/Http/Controllers/Auth/RiderController.php
@@ -2,19 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\BaseController;
-use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\Profile;
-use Illuminate\Foundation\Auth\RegistersUsers;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Support\Facades\Mail;
-use App\Mail\VerifyEmail;
-
+use App\Http\Controllers\BaseController;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RiderController extends BaseController
 {
